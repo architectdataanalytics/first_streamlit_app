a3081748b702ac90b8e926d8666246e2363dc71a
architectdataanalytics <122045426+architectdataanalytics@users.noreply.github.com>
2023-01-05 22:04:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #Picklist to pick a fruit.
-fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.Fruit),default=['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.'Fruit'),default=['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
