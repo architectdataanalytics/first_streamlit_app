b0cad71a237ac6a9456b259122f27a25933613d1
architectdataanalytics <122045426+architectdataanalytics@users.noreply.github.com>
2023-01-05 20:59:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
-import streamlit
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & BlueBerry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie');
-streamlit.text('Hard boiled Free-range egg')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
