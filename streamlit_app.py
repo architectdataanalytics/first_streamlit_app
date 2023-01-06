0f073fea75b41c09d5703e0af685dcd9cac0086b
architectdataanalytics <122045426+architectdataanalytics@users.noreply.github.com>
2023-01-06 13:41:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import pandas
 #🥣 🥗 🐔 🥑🍞
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑 Avacado Toast')
+streamlit.text('🥣🍞 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗🍞 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔🍞 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
